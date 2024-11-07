2909fa6ee644d55a22ba7b6390a13faf6567220c
Kate465 <kategokeborate@gmail.com>
2024-11-07 14:08:16 +0000

    added basic javascript to project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Fatshe Leno dawg",
+    autoStart: true,
+    delay: 1,
+    cursor: " ",
+  });
+
+  let poemElement = document.querySelector("#poem");
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
